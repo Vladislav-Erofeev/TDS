6aa24074baba73280267602ea189133c8f729ff7
Vladislav <20031911@mail.ru>
2024-02-11 13:50:41 +0300

    Initial commit

---
+++ src/main/java/vlad/erofeev/layerservice/LayerServiceApplication.java
@@ -0,0 +1,13 @@
+package vlad.erofeev.layerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LayerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vlad/erofeev/layerservice/LayerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vlad.erofeev.layerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LayerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
