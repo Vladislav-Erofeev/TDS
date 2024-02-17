39706fed42f6ac27ae1b576fe44e31173a8dc896
Vladislav <20031911@mail.ru>
2024-02-17 21:42:26 +0300

    feat: added TestContainers

--- src/test/java/vlad/erofeev/layerservice/LayerServiceApplicationTests.java
+++ src/test/java/vlad/erofeev/layerservice/LayerServiceApplicationTests.java
@@ -1,9 +1,47 @@
 package vlad.erofeev.layerservice;
 
+import org.junit.ClassRule;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
 class LayerServiceApplicationTests {
+
+    @ClassRule
+    public static PostgreSQLContainer postgres = new PostgreSQLContainer("postgres")
+            .withDatabaseName("test-db");
+
+    @BeforeAll
+    static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
+
+    @DynamicPropertySource
+    static void props(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Test
+    public void testContextLoad() {
+
+    }
 
 }
