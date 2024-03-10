9921a0b4a6fccb00c79a4cc58cfd62140f17e490
Vladislav <20031911@mail.ru>
2024-03-10 21:20:46 +0300

    feat: добавлен сервис обнаружения служб

--- layers-service/src/main/java/vlad/erofeev/layerservice/LayerServiceApplication.java
+++ layers-service/src/main/java/vlad/erofeev/layerservice/LayerServiceApplication.java
@@ -2,8 +2,10 @@ package vlad.erofeev.layerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class LayerServiceApplication {
 
     public static void main(String[] args) {
