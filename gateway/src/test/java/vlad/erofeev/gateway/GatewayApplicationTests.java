179befe5fb090baa9bc70380439d01fa77b1d37c
Vladislav <20031911@mail.ru>
2024-03-17 14:18:13 +0300

    feat: какие-то изменения

---
+++ gateway/src/test/java/vlad/erofeev/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package vlad.erofeev.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
