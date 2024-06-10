e60e796427f61ae327b694ba7e2d32959fb08da7
Vladislav <20031911@mail.ru>
2024-06-10 16:43:35 +0300

    feat: валидация типа файла

--- search-service/src/main/java/com/example/searchservice/controllers/GeocodingController.java
+++ search-service/src/main/java/com/example/searchservice/controllers/GeocodingController.java
@@ -1,15 +1,20 @@
 package com.example.searchservice.controllers;
 
 import com.example.searchservice.entities.GeocodedFile;
+import com.example.searchservice.exceptions.UnsupportedFormatException;
 import com.example.searchservice.services.GeocodedFileService;
 import com.example.searchservice.services.GeocodingService;
 import com.example.searchservice.services.NotificationService;
 import lombok.RequiredArgsConstructor;
+import org.apache.http.entity.ContentType;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import reactor.core.publisher.Flux;
@@ -28,7 +33,9 @@ public class GeocodingController {
 
     @PostMapping
     public void geocoding(@RequestPart("file") MultipartFile file,
-                                  @AuthenticationPrincipal OAuth2AuthenticatedPrincipal principal) throws IOException {
+                                  @AuthenticationPrincipal OAuth2AuthenticatedPrincipal principal) throws IOException, UnsupportedFormatException {
+        if (!file.getContentType().equals(ContentType.TEXT_PLAIN.getMimeType()))
+            throw new UnsupportedFormatException("unsupported format " + file.getContentType());
         geocodingService.saveFile(file, principal.getAttribute("id"));
     }
 
@@ -46,5 +53,10 @@ public class GeocodingController {
     @GetMapping
     public List<GeocodedFile> getAllByPersonId(@AuthenticationPrincipal OAuth2AuthenticatedPrincipal principal) {
         return geocodedFileService.getAllByPersonId(principal.getAttribute("id"));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> errorResponse(UnsupportedFormatException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
---
+++ search-service/src/main/java/com/example/searchservice/exceptions/UnsupportedFormatException.java
@@ -0,0 +1,7 @@
+package com.example.searchservice.exceptions;
+
+public class UnsupportedFormatException extends Exception{
+    public UnsupportedFormatException(String message) {
+        super(message);
+    }
+}
